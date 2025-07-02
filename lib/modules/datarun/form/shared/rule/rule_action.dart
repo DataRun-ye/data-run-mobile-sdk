295ea417cf11540604e270d1d0bf1405f21e2e39
Hamza-ye <7amza.it@gmail.com>
2025-07-03 01:16:37 +0300

    add rule actions

--- lib/modules/datarun/form/shared/rule/rule_action.dart
+++ lib/modules/datarun/form/shared/rule/rule_action.dart
@@ -1,29 +1,27 @@
+
+
 enum RuleActionType {
   // Expression must be logical (true, false)
   // Visibility,
-  Show, // deprecated, use Visibility
-  Hide, // deprecated, use Visibility
-  HideSection, // deprecated, use Visibility
-  Error, // deprecated, use Constraint
-  ErrorOnComplete, // deprecated, use Constraint
-  Warning, // deprecated, use Constraint
-  WarningOnComplete, // deprecated, use Constraint
-  Filter, // deprecated, use choice Filter
-  // Expression must be logical (true, false)
-  StopRepeat,
+  Show,
+  Hide,
+  Error,
+  Warning,
+  Filter,
   // Expression must be logical (true, false)
   Mandatory,
-  DisplayText,
-  DisplayKeyValuePair,
-
-  HideOption,
-  HideOptionGroup,
-  ShowOptionGroup,
-  // Expression result must be numerical >= 0
-  Count,
   // Expression result must be a compatible Value with the Field type
   // i.e for default Value
   Assign,
+
+  // new
+  WarningOnComplete, // deprecated, use Constraint
+  ErrorOnComplete, // deprecated, use Constraint
+  DisplayText,
+  DisplayKeyValuePair,
+  HideOption,
+  HideOptionGroup,
+  ShowOptionGroup,
   Unknown;
 
   static List<RuleActionType> get VISIBILITY_ACTIONS =>
@@ -31,29 +29,33 @@ enum RuleActionType {
 
   bool get isVisibility => VISIBILITY_ACTIONS.contains(this);
 
-  static RuleActionType getAction(String? action) {
-    switch (action?.toLowerCase()) {
-      case 'show':
-        return RuleActionType.Show;
-      case 'hide':
-        return RuleActionType.Hide;
-      case 'error':
-        return RuleActionType.Error;
-      case 'warning':
-        return RuleActionType.Warning;
-      case 'filter':
-        return RuleActionType.Filter;
-      case 'stopRepeat':
-        return RuleActionType.StopRepeat;
-      case 'mandatory':
-        return RuleActionType.Mandatory;
-      case 'count':
-        return RuleActionType.Count;
-      case 'assign':
-        return RuleActionType.Assign;
-      default:
-        return RuleActionType.Unknown;
-      // throw ArgumentError('Invalid value type: $valueType');
-    }
-  }
+  static RuleActionType getAction(String? action) => RuleActionType.values
+      .firstWhere((e) => e.name.toLowerCase() == action?.toLowerCase(),
+          orElse: () => RuleActionType.Unknown);
+
+  // static RuleActionType getAction(String? action) {
+  //   switch (action?.toLowerCase()) {
+  //     case 'show':
+  //       return RuleActionType.Show;
+  //     case 'hide':
+  //       return RuleActionType.Hide;
+  //     case 'error':
+  //       return RuleActionType.Error;
+  //     case 'warning':
+  //       return RuleActionType.Warning;
+  //     case 'filter':
+  //       return RuleActionType.Filter;
+  //     case 'mandatory':
+  //       return RuleActionType.Mandatory;
+  //     case 'assign':
+  //       return RuleActionType.Assign;
+  //     case 'assign':
+  //       return RuleActionType.Assign;
+  //     case 'assign':
+  //       return RuleActionType.Assign;
+  //     default:
+  //       return RuleActionType.Unknown;
+  //     // throw ArgumentError('Invalid value type: $valueType');
+  //   }
+  // }
 }
